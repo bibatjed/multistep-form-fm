0d43e99709d888b5a3e8d113f397d5f80b1c74b9
bibatjed <bibatjed@gmail.com>
2023-03-14 18:36:58 +0800

    Set up ubuntu font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'Georgia']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+  ]
 }
